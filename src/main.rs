a052bbaddeda624a83e648dbb85d5856559d358a
Valatras <arifcan091@gmail.com>
2026-09-15 14:46:12 +0200

    feature: temporary count function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 fn main() {
     greet("world");
+    count();
 }
 
 fn greet(name: &str) {
     println!("Hello, {}!", name);
+}
+
+fn count(){
+    let mut numbers = vec![10, 20, 30, 40];
+
+    /*
+    for number in numbers {
+        println!("{number}");
+    }
+     */
+    
+    numbers.push(4); 
+    numbers.push(5);
+
+    for number in numbers {
+        println!("{number}");
+    }
 }
