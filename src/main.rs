e1cd1d45d55266926292d9691667f4f5dfaf42c5
Valatras <arifcan091@gmail.com>
2026-09-18 03:44:34 +0200

    feature: mandelbrot window vizualization

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-// use num_complex::Complex;
-// use rust_mandelbrot_set_representation::*;
+use num_complex::Complex;
+use rust_mandelbrot_set_representation::*;
 use std::sync::Arc;
 use pixels::{Pixels, SurfaceTexture};
 use winit::{
@@ -10,8 +10,8 @@ window::Window,
 };
   
 //initial size of my window
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 600;
+const WIDTH: u32 = 1920;
+const HEIGHT: u32 = 1080;
   
 struct App {
 // here I create a window. The Arc option allows multiple accessing for ... ?
@@ -75,14 +75,7 @@ impl ApplicationHandler for App {
 		WindowEvent::RedrawRequested => {
 		let pixels = self.pixels.as_mut().unwrap();
 		let frame = pixels.frame_mut();
-		for pixel in frame.chunks_exact_mut(4) {
-		
-		// A pixel has four values (RGBA)
-		pixel[0] = 255; // Rouge
-		pixel[1] = 0; // Vert
-		pixel[2] = 0; // Bleu
-		pixel[3] = 255; // Alpha
-		}
+		draw_mandelbrot(frame);
 		// now we draw the pixel buffers on the window
 		pixels.render().unwrap();
 		}
@@ -92,6 +85,39 @@ impl ApplicationHandler for App {
 	}
 }
 
+
+fn draw_mandelbrot(frame: &mut [u8]){
+    let min_real = -3.0;
+    let max_real = 3.0;
+    let min_imaginary = -2.0;
+    let max_imaginary = 2.0;
+
+    for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+
+            let x_pos = (i % WIDTH as usize) as f32;
+            // we'll use / operator as the index grows in x. So Each row contains WIDTH pixels, and there are HEIGHT rows. 
+            // We still want an f32 result as we want a ratio to multiply with 255 the color intensity.
+            let y_pos = (i / WIDTH as usize) as f32;
+            //pixel is a &mut [u8] variable. 
+            // Transformation pixel -> plan complexe : position finale=début+pourcentage de progression×taille de l’intervalle
+            let real = min_real + (x_pos as f64 / WIDTH as f64) * (max_real - min_real);
+            let imaginary = max_imaginary - (y_pos as f64 / HEIGHT as f64) * (max_imaginary - min_imaginary);
+            let c = Complex::new(real, imaginary);
+            // On vérifie si le pixel appartient à mandelbrot
+            let inside = is_in_mandlebrot_monot(c, 30);
+            
+            if inside == true{
+                pixel[0] = 0;
+                pixel[1] = 0; // Vert
+                pixel[2] = 0; // Bleu
+            }else {    
+                pixel[0] = 255;
+                pixel[1] = 255; // Vert
+                pixel[2] = 255; // Bleu   
+            }
+            pixel[3] = 255; // Alpha
+		}
+}
   
 
 fn main() {
